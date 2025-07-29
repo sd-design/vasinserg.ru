03b03c88b7fb2f44c05adaaa689ed8673b01f163
Alex Solovey <alex@sd-design.tk>
2025-07-29 09:08:18 +0300

    some edition of css

--- resources/views/general.blade.php
+++ resources/views/general.blade.php
@@ -697,66 +697,7 @@
     </div>
 </section>
 
-<section class="blog-section section-padding bg-contain" style="background-image: url('assets/img/blog_bg.png')">
-    <div class="container">
-        <div class="row mb-30">
-            <div class="col-12 col-lg-12">
-                <div class="section-title text-center">
-                    <p>Latest News & Blog</p>
-                    <h1>Get Every Single Updates</h1>
-                </div>
-            </div>
-        </div>
-
-        <div class="row">
-            <div class="col-xl-4 col-md-6 col-12">
-                <div class="single-blog-card">
-                    <div class="blog-featured-thumb bg-cover" style="background-image: url('assets/img/blog1.jpg')"></div>
-                    <div class="content">
-                        <div class="post-author">
-                            <a href="news-details.html"><i class="fal fa-user-circle"></i> Michael K. Steven</a>
-                        </div>
-                        <h3><a href="news-details.html">Make Honest Design Work As For Digital Business.</a></h3>
-                        <div class="btn-link-share">
-                            <a href="news-details.html" class="theme-btn minimal-btn">read more <i class="fas fa-arrow-right"></i></a>
-                            <a href="#"><i class="fal fa-share-alt"></i></a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-xl-4 col-md-6 col-12">
-                <div class="single-blog-card active">
-                    <div class="blog-featured-thumb bg-cover" style="background-image: url('assets/img/blog2.jpg')"></div>
-                    <div class="content">
-                        <div class="post-author">
-                            <a href="news-details.html"><i class="fal fa-user-circle"></i> Salman Ahmed</a>
-                        </div>
-                        <h3><a href="news-details.html">Should You Do When A Webs Design Trend Becomes</a></h3>
-                        <div class="btn-link-share">
-                            <a href="news-details.html" class="theme-btn minimal-btn">read more <i class="fas fa-arrow-right"></i></a>
-                            <a href="#"><i class="fal fa-share-alt"></i></a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-xl-4 col-md-6 col-12">
-                <div class="single-blog-card">
-                    <div class="blog-featured-thumb bg-cover" style="background-image: url('assets/img/blog3.jpg')"></div>
-                    <div class="content">
-                        <div class="post-author">
-                            <a href="news-details.html"><i class="fal fa-user-circle"></i> Michael K. Steven</a>
-                        </div>
-                        <h3><a href="news-details.html">How To Space And Kern Punct Marks Other Symbols</a></h3>
-                        <div class="btn-link-share">
-                            <a href="news-details.html" class="theme-btn minimal-btn">read more <i class="fas fa-arrow-right"></i></a>
-                            <a href="#"><i class="fal fa-share-alt"></i></a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
+@include('/layout/blog')
 
 <section class="cta-banner">
     <div class="container-fluid bg-cover section-bg" style="background-image: url('assets/img/cta_bg1.png')">
@@ -766,8 +707,8 @@
                     <h1>Ready To Get Free Consulations For <br> Any Kind Of It Solutions ? </h1>
                 </div>
                 <div class="col-xl-5 col-12 text-center text-xl-right">
-                    <a href="contact.html" class="theme-btn mt-4 mt-xl-0">Get a quote <i class="fas fa-arrow-right"></i></a>
-                    <a href="services-details.html" class="ml-sm-3 mt-4 mt-xl-0 theme-btn minimal-btn">read more <i class="fas fa-arrow-right"></i></a>
+                    <a href="contact.html" class="theme-btn mt-4 mt-xl-0">Получить консультацию <i class="fas fa-arrow-right"></i></a>
+                    <a href="services-details.html" class="ml-sm-3 mt-4 mt-xl-0 theme-btn minimal-btn">узнать больше <i class="fas fa-arrow-right"></i></a>
                 </div>
             </div>
         </div>
---
+++ resources/views/layout/blog.blade.php
@@ -0,0 +1,60 @@
+<section class="blog-section section-padding bg-contain" style="background-image: url('assets/img/blog_bg.png')">
+    <div class="container">
+        <div class="row mb-30">
+            <div class="col-12 col-lg-12">
+                <div class="section-title text-center">
+                    <p>Мои мысли</p>
+                    <h1>Get Every Single Updates</h1>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-xl-4 col-md-6 col-12">
+                <div class="single-blog-card">
+                    <div class="blog-featured-thumb bg-cover" style="background-image: url('assets/img/blog1.jpg')"></div>
+                    <div class="content">
+                        <div class="post-author">
+                            <a href="news-details.html"><i class="fal fa-user-circle"></i> Michael K. Steven</a>
+                        </div>
+                        <h3><a href="news-details.html">Make Honest Design Work As For Digital Business.</a></h3>
+                        <div class="btn-link-share">
+                            <a href="news-details.html" class="theme-btn minimal-btn">read more <i class="fas fa-arrow-right"></i></a>
+                            <a href="#"><i class="fal fa-share-alt"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-4 col-md-6 col-12">
+                <div class="single-blog-card active">
+                    <div class="blog-featured-thumb bg-cover" style="background-image: url('assets/img/blog2.jpg')"></div>
+                    <div class="content">
+                        <div class="post-author">
+                            <a href="news-details.html"><i class="fal fa-user-circle"></i> Salman Ahmed</a>
+                        </div>
+                        <h3><a href="news-details.html">Should You Do When A Webs Design Trend Becomes</a></h3>
+                        <div class="btn-link-share">
+                            <a href="news-details.html" class="theme-btn minimal-btn">read more <i class="fas fa-arrow-right"></i></a>
+                            <a href="#"><i class="fal fa-share-alt"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-4 col-md-6 col-12">
+                <div class="single-blog-card">
+                    <div class="blog-featured-thumb bg-cover" style="background-image: url('assets/img/blog3.jpg')"></div>
+                    <div class="content">
+                        <div class="post-author">
+                            <a href="news-details.html"><i class="fal fa-user-circle"></i> Michael K. Steven</a>
+                        </div>
+                        <h3><a href="news-details.html">How To Space And Kern Punct Marks Other Symbols</a></h3>
+                        <div class="btn-link-share">
+                            <a href="news-details.html" class="theme-btn minimal-btn">read more <i class="fas fa-arrow-right"></i></a>
+                            <a href="#"><i class="fal fa-share-alt"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
